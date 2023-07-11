71fb75d2aeaca7ebfa41b1af845f380a4ed60c11
bakare-rasheed <bakarerasheed.a38@gmail.com>
2023-07-11 10:33:28 +0100

    my script

---
+++ app.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Move the application zip file to the same directory as the script
+mv /home/bakare/Downloads/2129_crispy_kitchen.zip ./2129_crispy_kitchen.zip
+
+echo '
+# Base image
+FROM ubuntu:latest
+
+# Update package lists and install Apache2
+RUN apt-get update && apt-get install -y apache2
+
+# Copy the application zip file to the container
+COPY ./2129_crispy_kitchen.zip /var/www/html/
+
+# Unzip the application within the container
+RUN apt-get install -y unzip
+RUN unzip /var/www/html/2129_crispy_kitchen.zip -d /var/www/html/
+
+# Expose port 80 for Apache
+EXPOSE 80
+
+# Start Apache service
+CMD ["apache2ctl", "-D", "FOREGROUND"]
+' > Dockerfile
+
+sudo docker build -t bakare-app0 .
+
+sudo docker run -d -p 80:80 bakare-app0
